3ba302458be37504f5d94a86a92ac88ecb4aba17
demirovict <demirovict@gmail.com>
2014-12-05 19:49:25 +0100

    Day 4

---
+++ iSpendd2/UsersViewController.h
@@ -0,0 +1,16 @@
+//
+//  UsersViewController.h
+//  iSpendd2
+//
+//  Created by Academy387 on 12/5/14.
+//  Copyright (c) 2014 Academy387. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "User.h"
+
+@interface UsersViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property NSArray *users;
+
+@end
